121bc8fe4940ca7d4e5a32a653b71fc200f65ad6
Ilya Medvedev <medved55rus@gmail.com>
2016-03-23 08:46:40 +0600

    remove rudiment annotation

--- src/main/java/medvedev/ilya/example/chat/server/controller/MessageController.java
+++ src/main/java/medvedev/ilya/example/chat/server/controller/MessageController.java
@@ -3,7 +3,6 @@ package medvedev.ilya.example.chat.server.controller;
 import medvedev.ilya.example.chat.server.model.ClientMessage;
 import medvedev.ilya.example.chat.server.model.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -11,7 +10,6 @@ import java.security.Principal;
 @Controller
 public class MessageController {
     @MessageMapping("message")
-    @SendTo("/topic/message")
     public Message receiveMessage(final Principal principal, final ClientMessage message) {
         final String name = principal.getName();
         final String text = message.getText();
